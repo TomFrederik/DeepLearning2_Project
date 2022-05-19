938f4645df7f1fde1372f23685d72ccb76ad6b78
Tom <tlieberum@outlook.de>
2022-05-19 07:17:49 +0200

    bug fixes

--- imagenette/generate_random_cf_data_slurm.sh
+++ imagenette/generate_random_cf_data_slurm.sh
@@ -7,7 +7,7 @@ TRUNC=1
 SUP_NAME="random"
 
 RUN_NAME="$SUP_NAME/train_cf"
-python generate_data.py --mode random --n_data $N_DATA_PER_CLASS --run_name $RUN_NAME --weights_path $WEIGHTS --truncation $TRUNC
+python generate_data.py --mode random --n_data $N_DATA --run_name $RUN_NAME --weights_path $WEIGHTS --truncation $TRUNC
 
 RUN_NAME="$SUP_NAME/val_cf"
-python generate_data.py --mode random --n_data $N_DATA_PER_CLASS --run_name $RUN_NAME --weights_path $WEIGHTS --truncation $TRUNC
+python generate_data.py --mode random --n_data $N_DATA --run_name $RUN_NAME --weights_path $WEIGHTS --truncation $TRUNC
